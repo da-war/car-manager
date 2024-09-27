f74599bbfe73f6657e2378515ae792c1a767a23d
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-09-27 14:26:30 +0500

    Crud

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -7,4 +7,43 @@ export class CarService {
     public async getCar() {
         return this.cars;
     }
+
+    public async postCar(car) {
+        return this.cars.push(car);
+    }
+
+    public async putCar(car) {
+        this.cars.map((carItem) => {
+            if (carItem.id === car.id) {
+                carItem = car;
+            }
+        });
+        return car;
+    }
+
+    public async deleteCarById(carId) {
+        const carIndex = this.cars.findIndex((car) => car.id === carId);
+        if (carIndex === -1) {
+            return 'Car not found';
+        }
+        this.cars.splice(carIndex, 1);
+        return 'Car deleted';
+    }
+
+    public async getCarById(carId) {
+        const car = this.cars.find((car) => car.id === carId);
+        if (!car) {
+            return 'Car not found';
+        }
+        return car;
+    }
+
+    public async getCarByBrand(brand) {
+        const car = this.cars.find((car) => car.brand === brand);
+        if (!car) {
+            return 'Car not found';
+        }
+        return car;
+    }
+
 }
