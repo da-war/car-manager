04a346ffba93d384b39bb7f99b8ad0b8e5d6d909
Rana Dawar Abdullah <imdawar090@gmail.com>
2024-09-27 14:12:52 +0500

    Get Car Route Done

---
+++ src/car/car.mock.ts
@@ -0,0 +1,42 @@
+export const CARS=[
+    {
+        id:1,
+        brand:'BMW',
+        model:'X6',
+        year:2015,
+        color:'Black',
+        price: 50000
+    },
+    {
+        id:2,
+        brand:'Mercedes',
+        model:'S500',
+        year:2016,
+        color:'White',
+        price: 60000
+    },
+    {
+        id:3,
+        brand:'Audi',
+        model:'A8',
+        year:2017,
+        color:'Gray',
+        price: 70000
+    },
+    {
+        id:4,
+        brand:'Ford',
+        model:'Mustang',
+        year:2018,
+        color:'Red',
+        price: 80000
+    },
+    {
+        id:5,
+        brand:'Ferrari',
+        model:'F8',
+        year:2019,
+        color:'Yellow',
+        price: 90000
+    },
+];
--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { CARS } from './car.mock';
 
 @Injectable()
-export class CarService {}
+export class CarService {
+    private cars = CARS;
+    public async getCar() {
+        return this.cars;
+    }
+}
